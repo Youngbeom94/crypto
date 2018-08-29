8bc67eb9e2e8eadc749c268503bec9ddd91d71f1
Ella Rose <python_pride@protonmail.com>
2018-08-28 22:17:19 -0400

    broken

--- designs/linear/sumofsquares/signtest3.py
+++ designs/linear/sumofsquares/signtest3.py
@@ -37,9 +37,8 @@ def generate_keypair(parameters=PARAMETERS):
     return public_key, private_key
      
 def sign(m, private_key, parameters=PARAMETERS):
-    # (aa + bb) * (cc + dd) = (ac + bd)^2 + (ad - bc)^2
-    #(aa + bb)r(cc + ddr) = (ac(sqrt(r)) - bdr)^2 + (adr + bc(sqrt(r)))^2    
-    #(aa + bb)rr(cc + ddrr) = (acr - bdrr)^2 + (adrr + bcr)^2        
+    # (aa + bb) * (cc + dd) = (ac + bd)^2 + (ad - bc)^2    
+    # (aa + bb)rr(cc + ddrr) = (acr - bdrr)^2 + (adrr + bcr)^2        
     a, b = private_key
     c = random_integer(parameters["c_size"])
     d = random_integer(parameters["d_size"])
@@ -61,6 +60,12 @@ def test_sign_verify():
     proof = sign(message, private_key)
     assert verify(proof, message, public_key) == True        
     
+    broken = "Ella smash!"
+    proof2 = proof[0], (proof[1] / h(message)) * h(broken)
+    if verify(proof2, broken, public_key):
+        print("Broken!")
+        raise SystemExit()
+        
     from crypto.designs.linear.homomorphic.latticebased.unittesting import test_sign_verify
     test_sign_verify("Quadratic sum signature test 3", generate_keypair, sign, verify, iterations=10000)
     
